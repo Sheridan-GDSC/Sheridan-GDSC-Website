1e6091162b379273eeaa3aea540b33a6c13b5ba1
mhserrao <maizy_serrao@outlook.com>
2021-12-05 22:49:52 -0500

    Adding contact-us template to nav-bar component

---
+++ src/components/common/layout/contact_us.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./contact_us.css"
+
+const ContactUs = () => {
+    return (
+        <div className="cu">
+            <h1 className="cu-heading">Get in touch</h1>
+            <p className="cu-desc">Praesent mattis metus sit amet justo mattis eleifend. Suspendisse vel neque porta, tincidunt augue id, rhoncus dui. Sed molestie posuere nibh. In placerat est nibh, quis imperdiet quam iaculis et. Sed feugiat justo nec pharetra pharetra. Donec tortor augue, pharetra id.</p>
+        </div>
+    )
+}
+
+export default ContactUs
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,15 @@
 import * as React from "react"
 import NavBar from "../components/common/layout/nav-bar.js"
+import ContactUs from "../components/common/layout/contact_us.js"
 
 
 // markup
 const IndexPage = () => {
   return (
+  <div>
       <NavBar></NavBar>
+      <ContactUs></ContactUs>
+  </div>
   )
 }
 
