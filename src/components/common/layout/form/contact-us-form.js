c1f84ff89c3953bc80116120249eb46f95c133b4
sbelbeck <belbeck.sam@gmail.com>
2021-12-28 14:01:21 -0500

    style updates, cu: add gear/circle background icons

--- src/components/common/layout/form/contact-us-form.js
+++ src/components/common/layout/form/contact-us-form.js
@@ -8,6 +8,9 @@ import WomanTop from "../../../../../static/images/woman_on_computer.png"
 import ManTop from "../../../../../static/images/man_pointing_down.png"
 import ManBottom from "../../../../../static/images/man_holding_pencil.png"
 import WomanBottom from "../../../../../static/images/woman_carrying_picture.png"
+import SmallGear from "../../../../../static/images/small_gear.png"
+import LargeGear from "../../../../../static/images/large_gear.png"
+import LargeYlwGear from "../../../../../static/images/large_yellow_gear.png"
 
 const ContactUsForm = () => {
     const options = ["1","2", "3"]
@@ -32,22 +35,13 @@ const ContactUsForm = () => {
             <TextArea id="message" fieldName="Message: " isRequired="true" />
 						<Button id="submit" text="Submit" onClick={formSubmit}/>
 
-
-						<div id="womanTop" className="personIcon">
-              <img alt="cartoon girl typing on computer" src={WomanTop}></img>
-						</div>
-
-						<div id="manTop" className="personIcon">
-              <img alt="cartoon man pointing downward" src={ManTop}></img>
-						</div>
-
-						<div id="manBottom" className="personIcon">
-              <img alt="cartoon man holding a large pencil" src={ManBottom}></img>
-						</div>
-						
-						<div id="womanBottom" className="personIcon">
-              <img alt="cartoon woman carrying a picture" src={WomanBottom}></img>
-						</div>
+              <img id="womanTop" className="icons" alt="cartoon girl typing on computer" src={WomanTop} />
+              <img id="manTop" className="icons" alt="cartoon man pointing downward" src={ManTop} />	
+              <img id="manBottom" className="icons" alt="cartoon man holding a large pencil" src={ManBottom} />
+              <img id="womanBottom" className="icons" alt="cartoon woman carrying a picture" src={WomanBottom} />
+              <img id="small_gear" className="icons" alt="small gear icon" src={SmallGear} />
+              <img id="large_gear" className="icons" alt="large gear icon" src={LargeGear} />
+              <img id="large_ylw_gear" className="icons" alt="large yellow gear icon" src={LargeYlwGear} />
         </div>
     )
 }
