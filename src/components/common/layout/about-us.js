0646dc1b427bbc1385223087edbda51b503b36a7
sbelbeck <belbeck.sam@gmail.com>
2022-01-02 13:35:30 -0500

    add decription bubble components, add red loopy background

--- src/components/common/layout/about-us.js
+++ src/components/common/layout/about-us.js
@@ -3,6 +3,9 @@ import Stat from "./stat/stat"
 import GCircle from "../../../../static/images/green_circle.png"
 import BranchLeaves from "../../../../static/images/branch-leaves.png"
 import "./about-us.css"
+import DescriptionBubble from "./DescriptionBubble/DescriptionBubble";
+import RedLoop from "../../../../static/images/red_loops.png"
+import Polygon from "../../../../static/images/polygon.png"
 
 const AboutUs = () => {
 
@@ -10,18 +13,21 @@ const AboutUs = () => {
 	return (
 		<div className="AboutUs">
 
-			<h1 className="au-heading">
-			About Us
-				<div id="au-underline"></div>
-			</h1>
-
 			<div class="au-grid">
-
 				<div id="extraDetail">
-					ppl icons and title/info blurbs
+				<DescriptionBubble id="db1" title="Title" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolorem? Quos quod alias sunt ut laboriosam omnis a, sapiente veritatis." />
+				<DescriptionBubble id="db2" title="Title" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolorem? Quos quod alias sunt ut laboriosam omnis a, sapiente veritatis." />
+				<DescriptionBubble id="db3" title="Title" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolorem? Quos quod alias sunt ut laboriosam omnis a, sapiente veritatis." />
 				</div>
 
 				<div id="info">
+					<img id="redLoops" alt="grey polygon shape" src={RedLoop} /> 
+					{/* <img id="polygon" alt="small triangle shape" src={Polygon}></img> */}
+					<h1 className="au-heading">
+						About Us
+						<div id="au-underline"></div>
+					</h1>
+
 					<h2 id="subtitle">Preaesent mattis metus sit amet <span class="green">justo mattis.</span></h2>
 					<p id="au-blurb">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi quasi reiciendis libero! Provident cumque natus animi ipsam ratione, impedit quas laudantium iste blanditiis asperiores necessitatibus soluta odit dignissimos corporis assumenda optio cum eligendi minima ex molestiae aliquam amet magni. Placeat.</p>
 					<div id="statContainer">
@@ -29,12 +35,13 @@ const AboutUs = () => {
 						<Stat number="128" name="Members" />
 						<Stat number="40" name="Current Projects" />
 					</div>
-					<img id="branchLeaves" alt="cartoon branch with some leaves on it " src={BranchLeaves} />
+					<img id="branchLeaves" alt="cartoon branch with some leaves on it" src={BranchLeaves} />
 				</div>
 			</div>
 
-
 			<img id="greenIcon" alt="green circle" src={GCircle} />
+ 
+
 		</div>
 	)
 }
