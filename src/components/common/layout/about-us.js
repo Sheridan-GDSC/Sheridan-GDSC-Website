215228c7cafa2b77f437971bba5d13b4804225a8
sbelbeck <belbeck.sam@gmail.com>
2022-01-01 22:27:29 -0500

    create stat component and update left side about us component/styles

--- src/components/common/layout/about-us.js
+++ src/components/common/layout/about-us.js
@@ -1,12 +1,40 @@
 import React from "react";
-
+import Stat from "./stat/stat"
+import GCircle from "../../../../static/images/green_circle.png"
+import BranchLeaves from "../../../../static/images/branch-leaves.png"
+import "./about-us.css"
 
 const AboutUs = () => {
 
 
 	return (
 		<div className="AboutUs">
-			
+
+			<h1 className="au-heading">
+			About Us
+				<div id="au-underline"></div>
+			</h1>
+
+			<div class="au-grid">
+
+				<div id="extraDetail">
+					ppl icons and title/info blurbs
+				</div>
+
+				<div id="info">
+					<h2 id="subtitle">Preaesent mattis metus sit amet <span class="green">justo mattis.</span></h2>
+					<p id="au-blurb">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi quasi reiciendis libero! Provident cumque natus animi ipsam ratione, impedit quas laudantium iste blanditiis asperiores necessitatibus soluta odit dignissimos corporis assumenda optio cum eligendi minima ex molestiae aliquam amet magni. Placeat.</p>
+					<div id="statContainer">
+						<Stat number="56" name="Projects" />
+						<Stat number="128" name="Members" />
+						<Stat number="40" name="Current Projects" />
+					</div>
+					<img id="branchLeaves" alt="cartoon branch with some leaves on it " src={BranchLeaves} />
+				</div>
+			</div>
+
+
+			<img id="greenIcon" alt="green circle" src={GCircle} />
 		</div>
 	)
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 import * as React from "react"
 import NavBar from "../components/common/layout/nav-bar.js"
 import ContactUs from "../components/common/layout/contact_us.js"
+import AboutUs from "../components/common/layout/about-us.js"
 import "./index.css"
 
 
@@ -10,6 +11,7 @@ const IndexPage = () => {
   return (
   <div className="page-body">
       <NavBar></NavBar>
+			<AboutUs></AboutUs>
       <ContactUs></ContactUs>
   </div>
   )
