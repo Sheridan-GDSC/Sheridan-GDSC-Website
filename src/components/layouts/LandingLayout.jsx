1461728d77133d8274e10b165aad0ab03e678430
Jacob Brasil <jnxtbrasil@gmail.com>
2022-01-12 19:36:38 -0500

    Added lots of Breakpoints and new Components

--- src/components/layouts/LandingLayout.jsx
+++ src/components/layouts/LandingLayout.jsx
@@ -10,6 +10,10 @@ const LandingLayout = styled.div`
   height: 100vh;
   width: 100vw;
   z-index: 1;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
 `;
 
 export const LandingPadding = styled.div`
@@ -17,6 +21,10 @@ export const LandingPadding = styled.div`
   flex-direction: inherit;
   z-index: inherit;
   padding: 6.5vh 5vw;
+
+  @media (max-width: 768px) {
+    padding: 0;
+  }
 `;
 
 export const LandingBackground = styled.div`
@@ -29,6 +37,12 @@ export const LandingBackground = styled.div`
   height: 100vh;
   width: 100vw;
   z-index: 0;
+
+  pointer-events: none;
+
+  @media (max-width: 768px) {
+    display: none;
+  }
 `;
 
 export const LandingForeground = styled.div`
@@ -42,6 +56,8 @@ export const LandingForeground = styled.div`
   width: 100vw;
   z-index: 2;
   align-items: flex-end;
+
+  pointer-events: none;
 `;
 
 export default LandingLayout;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,44 +7,82 @@ import LandingLayout, {
   LandingForeground,
   LandingPadding,
 } from "../components/layouts/LandingLayout";
+import Calendar from "../components/common/Calendar";
+import HStack from "../components/common/layout/HStack";
+import VStack from "../components/common/layout/VStack";
+import SmallEventCard from "../components/common/SmallEventCard";
+import EventCard from "../components/common/EventCard";
 
 const ContentLeft = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
+
+  @media (max-width: 768px) {
+    position: absolute;
+    height: 100vh;
+    width: 100%;
+    text-align: center;
+    align-items: center;
+    overflow: hidden;
+  }
 `;
 
-const ContentRight = styled.div``;
+const ContentRight = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin: 0 0 3vh 17vw;
+  justify-content: center;
+  align-items: center;
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
 
 const Title = styled.h1`
   font-family: "Open Sans";
   font-weight: 700;
-  font-size: 3.875rem;
+  font-size: 5rem;
   margin: 0;
   padding: 0;
   letter-spacing: 0;
   color: #1473e6;
+
+  @media (max-width: 768px) {
+    font-size: 3rem;
+  }
 `;
 
 const Subtitle = styled.h2`
   font-family: "Open Sans";
   font-weight: 400;
-  font-size: 2.156rem;
-  margin: 0;
+  font-size: 3rem;
+  margin: -2vh 0 0 0;
   padding: 0;
   letter-spacing: 0;
   color: #5ead65;
+
+  @media (max-width: 768px) {
+    font-size: 1.5rem;
+    margin: 0;
+  }
 `;
 
 const Content = styled.p`
   font-family: "Poppins";
-  font-weight: 300;
-  font-size: 0.85rem;
+  font-weight: 200;
+  font-size: 1.15rem;
   color: #797d81;
   margin: 2.78vh 0;
   padding: 0;
   max-width: 30vw;
   word-wrap: break-word;
+
+  @media (max-width: 768px) {
+    max-width: 75vw;
+    font-size: 0.95rem;
+  }
 `;
 
 const CTAButton = styled.button`
@@ -65,7 +103,23 @@ const CTAButton = styled.button`
 `;
 
 const ForeImg = styled.div`
-  width: 68vw;
+  width: 63vw;
+  max-height: 558px;
+  overflow: hidden;
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const ForeImgMobile = styled.div`
+  display: none;
+
+  @media (max-width: 768px) {
+    display: block;
+    width: 100vw;
+    overflow: hidden;
+  }
 `;
 
 const IndexPage = () => {
@@ -89,6 +143,15 @@ const IndexPage = () => {
           )
         }
       }
+      fgm: file(name: { eq: "ForegroundMobile" }, extension: { eq: "png" }) {
+        childImageSharp {
+          gatsbyImageData(
+            placeholder: TRACED_SVG
+            layout: CONSTRAINED
+            quality: 100
+          )
+        }
+      }
     }
   `);
 
@@ -97,7 +160,7 @@ const IndexPage = () => {
       <LandingBackground>
         <GatsbyImage
           image={data.bg.childImageSharp.gatsbyImageData}
-          objectFit="cover"
+          // objectFit="cover"
         />
       </LandingBackground>
       <LandingLayout>
@@ -113,16 +176,43 @@ const IndexPage = () => {
             </Content>
             <CTAButton>General Member Application</CTAButton>
           </ContentLeft>
-          <ContentRight></ContentRight>
+          <ContentRight>
+            <VStack>
+              <HStack>
+                <Calendar month="October" activeDate={4} startingDay={4} />
+                <VStack>
+                  <SmallEventCard
+                    title="Tech Interview 101"
+                    date="Oct 7, 2021"
+                    color="#5EAD65"
+                  />
+                  <SmallEventCard
+                    title="Android Study Jam"
+                    date="Oct 17, 2021"
+                  />
+                </VStack>
+              </HStack>
+              <EventCard
+                title="Create Series - Introduction to GoLang & APIs"
+                date="Oct 21, 2021"
+              />
+            </VStack>
+          </ContentRight>
         </LandingPadding>
       </LandingLayout>
       <LandingForeground>
         <ForeImg>
           <GatsbyImage
             image={data.fg.childImageSharp.gatsbyImageData}
-            objectFit="cover"
+            objectFit="fit"
           />
         </ForeImg>
+        <ForeImgMobile>
+          <GatsbyImage
+            image={data.fgm.childImageSharp.gatsbyImageData}
+            objectFit="fit"
+          />
+        </ForeImgMobile>
       </LandingForeground>
     </>
   );
