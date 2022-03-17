4c60bbe66bb4c23a5074c3ade202a9546c7a02a1
Andres Munevar <afmunevarm@hotmail.com>
2022-03-16 23:41:24 -0400

    Add content into all layouts

--- src/services/content.js
+++ src/services/content.js
@@ -3,7 +3,11 @@ export const content = {
     main: "Welcome to the Sheridan College chapter of Google Developer Student Clubs. Join the club and attend events to learn about Google developer technologies, take part in Google's Solution Challenges, and explore the world of software development."
   },
   about: {
-    main: "Google Developer Student Clubs (GDSC) is a student-run club where we explore Google technologies such as Google Cloud and Android, learn about programming languages like Python and Java, and discover all aspects of software development. Meet other Sheridan College students to gain and share experiences about studying software development at Sheridan College. Not sure what to expect during your co-op work terms? What about LeetCode and what to do with it? Or maybe how to ace interviews and land that internship? Our club leads have been through it all so don’t miss out on these exclusive events. Join our <link>Discord</link> to connect with other club members and stay up to date on all that’s happening. We welcome all Sheridan College students!    ",
+    main: {
+      first: "Google Developer Student Clubs (GDSC) is a student-run club where we explore Google technologies such as Google Cloud and Android, learn about programming languages like Python and Java, and discover all aspects of software development.",
+      second: "Meet other Sheridan College students to gain and share experiences about studying software development at Sheridan College. Not sure what to expect during your co-op work terms? What about LeetCode and what to do with it? Or maybe how to ace interviews and land that internship? Our club leads have been through it all so don’t miss out on these exclusive events.",
+      third: "Join our <a target=\"_blank\" href=\"https://discord.gg/cfQDGtkeWG\">Discord</a> to connect with other club members and stay up to date on all that’s happening. We welcome all Sheridan College students!"
+    },
     bubbleOne: {
       title: "Hands-on Workshops",
       main: "Keep an eye out for our Hands-on Workshops where a club lead will take you through topics including building projects from scratch."
@@ -12,7 +16,7 @@ export const content = {
       title: "Study Jams",
       main: "In our Study Jams, we follow a curriculum set by Google Developer Experts to learn about Google technologies such as Android and Google Cloud."
     },
-    bubblethree: {
+    bubbleThree: {
       title: "Speaker Sessions",
       main: "We stand on the shoulders of giants. Gain valuable knowledge from our speakers on everything from programming tips and tricks to life as a student programmer."
     }
